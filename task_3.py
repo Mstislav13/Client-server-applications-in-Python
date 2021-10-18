cf38a4fa5610f47d006bc88b9d0a4b274346922a
mstislav <strong13strong@gmail.com>
2021-10-18 20:41:39 +0700

    hw commit

---
+++ task_3.py
@@ -0,0 +1,17 @@
+# 3. Определить, какие из слов «attribute», «класс», «функция», «type»
+#    невозможно записать в байтовом типе.
+
+fir_word = b'attribute'
+print(fir_word)
+
+# sec_word = b'класс'
+# print(sec_word)
+
+# thr_word = b'функция'
+# print(thr_word)
+
+fou_word = b'type'
+print(fou_word)
+
+# Слова записанные на кириллице вызывают ошибку:
+# SyntaxError: bytes can only contain ASCII literal characters.
