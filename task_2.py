ef2b782a18a60f51c065ef0676cfcf5352db27ac
Mstislav13 <71246531+Mstislav13@users.noreply.github.com>
2021-11-01 17:05:13 +0700

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -62,7 +62,19 @@ def main():
 
     with open('orders.json', 'w') as file:
         json.dump(orders, file, indent=4, ensure_ascii=False)
-
+      
+      
+# with open('orders_1.json', 'w', encoding='utf-8') as f_in:
+# orders_list = data['orders']
+# order_info = {
+# 'item': item,
+# 'quantity': quantity,
+# 'price': price,
+# 'buyer': buyer,
+# 'date': date
+# }
+# orders_list.append(order_info)
+# json.dump(data, f_in, indent=4)
 
 if __name__ == '__main__':
     main()
